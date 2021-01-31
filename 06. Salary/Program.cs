c2f0d14204d89c0a64fafaca3e17c6071828b4a8
Момчил Миланов <deadman_kula@abv.bg>
2021-01-31 17:26:10 +0200

    q

--- 06. Salary/Program.cs
+++ 06. Salary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata;
 
 namespace _06._Salary
 {
@@ -6,7 +8,36 @@ namespace _06._Salary
     {
         static void Main(string[] args)
         {
-
+            const int facebook = 150;
+            const int instagram = 100;
+            const int reddit = 50;
+            int n = int.Parse(Console.ReadLine());
+            int sallary = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string site = Console.ReadLine();
+                switch (site)
+                {
+                    case "Facebook":
+                        sallary -= facebook;
+                        break;
+                    case "Instagram":
+                        sallary -= instagram;
+                        break;
+                    case "Reddit":
+                        sallary -= reddit;
+                        break;
+                }
+                if (sallary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            if (sallary > 0)
+            {
+                Console.WriteLine(sallary);
+            }
         }
     }
 }
