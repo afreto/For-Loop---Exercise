e3d64c12db80df11e2a82ec4491493b981adf3fe
Момчил Миланов <deadman_kula@abv.bg>
2021-01-31 10:53:35 +0200

    q

--- 01. Numbers Ending in 7/Program.cs
+++ 01. Numbers Ending in 7/Program.cs
@@ -6,7 +6,12 @@ namespace _01._Numbers_Ending_in_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 7; i < 1000; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
