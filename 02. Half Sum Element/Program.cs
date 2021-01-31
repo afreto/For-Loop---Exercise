1c5b0aba393e3864170c5904c15a0988854795d5
Момчил Миланов <deadman_kula@abv.bg>
2021-01-31 11:19:18 +0200

    q

--- 02. Half Sum Element/Program.cs
+++ 02. Half Sum Element/Program.cs
@@ -8,19 +8,26 @@ namespace _02._Half_Sum_Element
         {
             int n = int.Parse(Console.ReadLine());
             int maxVal = int.MinValue;
-
-
-
+            int sum = 0;
             for (int i = 0; i < n; i++)
             {
                 int num = int.Parse(Console.ReadLine());
-
-
-
-
-
-
-
+                sum += num;
+                if (num > maxVal)
+                {
+                    maxVal = num;
+                }
+            }
+            int sumWithoutMax = sum - maxVal;
+            if (sumWithoutMax == maxVal)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxVal}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxVal - sumWithoutMax)}");
             }
         }
     }
